f76133bf45a298b66bb40689dc97b10412b10669
Alexandr <ak4raulov@yandex.ru>
2021-07-08 17:31:16 +0300

    добавил запросы в базу данных

--- scripts/script.js
+++ scripts/script.js
@@ -8,5 +8,81 @@ headerCityButton.addEventListener('click', () => {
     localStorage.setItem('lomoda-location', city);
 });
 
+// Блокировка скролла
+const disableScroll = () => {
+	const widthScroll = window.innerWidth - document.body.offsetWidth;
+		document.body.dbScrollY = window.scrollY;
+		document.body.style.cssText = `
+			position: fixed;
+			top: ${-window.scrollY}px;
+			left: 0;
+			width: 100%;
+			height: 100vh;
+			overflow: hidden;
+			padding-right: ${widthScroll}px;
+	`;
+};
 
+const enableScroll = () => {
+	document.body.style.cssText = '';
+	window.scroll({
+		top: document.body.dbScrollY,
+	});
+};
 // Модальное окно
+
+const subheaderCart = document.querySelector('.subheader__cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+
+const cartModalOpen = () => {
+	cartOverlay.classList.add('cart-overlay-open');
+	disableScroll();
+};
+
+const cartModalClose = () => {
+	cartOverlay.classList.remove('cart-overlay-open');
+	enableScroll();
+};
+
+// закрыть модальное окно по esc
+document.addEventListener('keydown', event => {
+    if (event.code === 'Escape') { 
+        cartModalClose();
+    }
+});
+
+// запрос базы данных
+
+const getData = async () => {
+	const data = await fetch('db.json');
+
+	if (data.ok) {
+		return data.json();
+	} else {
+		throw new Error('данные не были получены, ошибка $(data.status) ${data.statusText}');
+	}
+};
+
+const getGoods = (callback) => {
+	getData()
+		.then(data => {
+			callback(data);
+		})
+		.catch(err => {
+			console.error(err);
+		});
+};
+
+getGoods((data) => {
+	console.warn(data);
+});
+
+subheaderCart.addEventListener('click', cartModalOpen);
+
+cartOverlay.addEventListener('click',  event => {
+	const target = event.target;
+	if (target.matches('.cart__btn-close') || target.matches('.cart-overlay'))  {
+		cartModalClose();
+	}
+});
+
